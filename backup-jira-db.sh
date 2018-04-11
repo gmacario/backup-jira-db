d6b1e7622a06b62869d75a0d6afe2c9880ff557f
Gianpaolo Macario <gmacario@gmail.com>
2018-04-11 07:10:10 +0200

    Add ideas how to use docker-volume-container-sync
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

--- backup-jira-db.sh
+++ backup-jira-db.sh
@@ -3,9 +3,9 @@
 set -x
 set -e
 
-# CID="hopeful_hermann"
-CID=$(docker ps | awk '/atlassian-jira-software/ {print $1}')
-echo "DEBUG: CID=${CID}"
+# JIRA_CID="hopeful_hermann"
+JIRA_CID=$(docker ps | awk '/atlassian-jira-software/ {print $1}')
+echo "DEBUG: JIRA_CID=${JIRA_CID}"
 
 DESTDIR='bk-itmgmacariow7-jira'
 
@@ -19,10 +19,17 @@ fi
 NOW=$(date '+%Y-%m-%d %H:%M:%S')
 cd "${DESTDIR}"
 
-# See https://github.com/nabeken/docker-volume-container-rsync
-docker exec "${CID}" sh -c "id; id; du -sh data export"
+docker exec "${JIRA_CID}" sh -c "id; id; du -sh data export"
 
+# TODO: See https://github.com/nabeken/docker-volume-container-rsync
+# RSYNC_CID=$(docker run -d -p 10873:873 nabeken/docker-volume-container-rsync:latest)
+# docker run -it --volumes-from ${RSYNC_CID} ubuntu /bin/bash
 # TODO: rsync -avz "${SOURCEDIR}" .
+# TODO: rsync rsync://<docker>:10873/
+# TODO: rsync -avP /path/to/dir rsync://<docker>:10873/volume/
+# ...
+# docker stop ${RSYNC_CID}
+
 find . -type f -exec chmod -x {} \;
 git add -A
 git commit -m "Mirrored on ${NOW}
